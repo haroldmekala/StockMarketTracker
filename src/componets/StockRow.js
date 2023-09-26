0fb95ac2c5ac280b373e387fcf301700f8f00b7a
Harold Mekala <harold.mekala@gmail.com>
2023-09-26 18:39:45 -0500

    Roughed-out how it look

--- src/componets/StockRow.js
+++ src/componets/StockRow.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'; 
 
 class StockRow extends Component {
+
+constructor(props){
+    super (props)
+this.state = {
+    price: 5,
+    date: '12-12-12',
+    time: '09:45'
+}
+}
 render (){
-    return <tr>
+    return (
+    <tr>
         <td>{this.props.ticker}</td>
-        <td>asf</td>
-        <td>asf</td>
-        <td>asf</td>
+        <td>{this.state.price}</td>
+        <td>{this.state.date}</td>
+        <td>{this.state.time}</td>
     </tr>
+    )
 }
 }
 
