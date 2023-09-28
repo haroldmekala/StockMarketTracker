1fa8c76e56343e341cf729a48be79a00cb7af927
Harold Mekala <harold.mekala@gmail.com>
2023-09-28 12:23:40 -0500

    Last working table

--- src/componets/StockRow.js
+++ src/componets/StockRow.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'; 
-
 class StockRow extends Component {
 
 constructor(props){
     super (props)
 this.state = {
-    price: 5,
-    date: '12-12-12',
-    time: '09:45'
+    data: {}
+    }
 }
+
+componentDidMount(){
+    //query the API
+    this.setState({
+    data:{
+        price:100.00,
+        date: '00-00-00',
+        time: '00:00'
+    }
+    })
 }
+  
 render (){
     return (
     <tr>
         <td>{this.props.ticker}</td>
-        <td>{this.state.price}</td>
-        <td>{this.state.date}</td>
-        <td>{this.state.time}</td>
+        <td>{this.state.data.price}</td>
+        <td>{this.state.data.date}</td>
+        <td>{this.state.data.time}</td>
     </tr>
     )
 }
