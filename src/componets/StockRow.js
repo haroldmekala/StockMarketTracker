d15d47eead84b9b47410eeec4690e1da9441e99b
Harold Mekala <harold.mekala@gmail.com>
2023-10-10 15:36:44 -0500

    first commit

--- src/componets/StockRow.js
+++ src/componets/StockRow.js
@@ -12,13 +12,13 @@ this.state = {
 componentDidMount() {
     //query the API
     const url2 = `${iex.base_url}/core/${this.props.ticker}/intraday_prices/token=${iex.api_token}`
-    console.log("url2 ="+url2)
+    console.log("url2 = " + url2)
     const url1 = "https://api.iex.cloud/v1/data/core/intraday_prices/spy?range=1d&token=pk_0c730bfd08784eacb6c3c9c17916ad57"
-    console.log("url1 ="+url1)
+    console.log("url1 = " + url1)
     fetch(url1)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
+        console.log(data)e
         this.setState ({
             data:data[data.length - 1]
         })
